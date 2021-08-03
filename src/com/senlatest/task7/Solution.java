b8d30d93aad7d9984ae8e113e40e309260350d48
ReznikElena <73893633+ILLI1979@users.noreply.github.com>
2021-08-03 20:02:25 +0300

    Add files via upload

--- src/com/senlatest/task7/Solution.java
+++ src/com/senlatest/task7/Solution.java
@@ -1,4 +1,4 @@
-package com.senlatest.task7;
+package com.example.senlatest.task7;
 
 import java.util.ArrayList;
 
@@ -12,5 +12,48 @@ import java.util.ArrayList;
 
  */
 public class Solution {
+}
 
+import java.util.ArrayList;
+
+public class PackBag {
+
+    public static void Pack(ArrayList<Integer> weights, ArrayList<Integer> costs, int capacity){
+
+        int NB_ITEMS = weights.size();
+        int[][] matrix = new int[NB_ITEMS + 1][capacity + 1];
+
+        for (int i = 0; i <= capacity; i++)
+            matrix[0][i] = 0;
+
+        for (int i = 1; i <= NB_ITEMS; i++) {
+            for (int j = 0; j <= capacity; j++) {
+                if (weights.get(i - 1) > j)
+                    matrix[i][j] = matrix[i-1][j];
+                else
+                    matrix[i][j] = Math.max(matrix[i-1][j], matrix[i-1][j - weights.get(i-1)] +
+                            costs.get(i-1));
+            }
+        }
+
+        int res = matrix[NB_ITEMS][capacity];
+        int w = capacity;
+        ArrayList<Integer> weightsSolution = new ArrayList<>();
+        ArrayList<Integer> costsSolution = new ArrayList<>();
+
+        for (int i = NB_ITEMS; i > 0  &&  res > 0; i--) {
+            if (res != matrix[i-1][w]) {
+                weightsSolution.add(weights.get(i-1));
+                costsSolution.add(costs.get(i-1));
+
+                res -= costs.get(i-1);
+                w -= weights.get(i-1);
+            }
+        }
+        System.out.println("Include: ");
+        for (int i = 0; i < weightsSolution.size(); i++){
+            System.out.println( "Weight: " + weightsSolution.get(i) + "  Cost: " + costsSolution.get(i));
+        }
+        System.out.println();
+    }
 }
